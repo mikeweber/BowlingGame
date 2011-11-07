078df91f4b14efc94077146620d831787b81ad9e
Mike Weber <mike@weberapps.com>
2011-11-07 10:18:38 -0600

    Move the "run" method into the GamePresenter

--- app/game_presenter.rb
+++ app/game_presenter.rb
@@ -5,6 +5,21 @@ class GamePresenter
     @game = game
   end
   
+  def run
+    last_roll = nil
+    while !@game.finished?
+      puts "What did you roll for the #{@game.current_frame.first_roll.nil? ? 'first' : (@game.current_frame.second_roll.nil? ? 'second' : 'third')} roll of frame #{@game.current_frame.frame}?"
+      last_roll = gets.gsub("\n", '')
+      break if last_roll == "exit"
+      begin
+        @game.roll(last_roll.to_i)
+        self.print_score_card
+      rescue InvalidRoll => e
+        puts "You entered an invalid roll: #{last_roll}. Try again."
+      end
+    end
+  end
+  
   def print_score_card
     s  = print_top_line
     s << print_frame_numbers
--- app/main.rb
+++ app/main.rb
@@ -1,19 +1,3 @@
+$: << File.expand_path(File.join(File.dirname(__FILE__)))
 require 'game'
 require 'game_presenter'
-
-def run
-  game = Game.new
-  
-  last_roll = nil
-  while !game.finished?
-    puts "What did you roll for the #{game.current_frame.first_roll.nil? ? 'first' : (game.current_frame.second_roll.nil? ? 'second' : 'third')} roll of frame #{game.current_frame.frame}?"
-    last_roll = gets.gsub("\n", '')
-    break if last_roll == "exit"
-    begin
-      game.roll(last_roll.to_i)
-      GamePresenter.new(game).print_score_card
-    rescue InvalidRoll => e
-      puts "You entered an invalid roll: #{last_roll}. Try again."
-    end
-  end
-end
